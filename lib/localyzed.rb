09d72782ad213ce9aac777a120aaac99530849ed
NicoArbogast <nicolas.arbogast@gmail.com>
2013-08-28 11:11:42 +0200

    bundle udpate + bump v

--- lib/localyzed.rb
+++ lib/localyzed.rb
@@ -1,3 +1,4 @@
+require 'rails-i18n'
 module Localyzed
 end
 require 'localyzed/base'
--- lib/localyzed/base.rb
+++ lib/localyzed/base.rb
@@ -4,7 +4,22 @@ module Localyzed
   end
 
   def self.localyze_routes(translations_file,options={})
+    ensure_translations_file_exists(translations_file)
     ActionDispatch::Routing::Translator.translate_from_file(translations_file, options)
+  end
+  
+  def self.ensure_translations_file_exists translations_file
+    translate_filepath = Rails.root.join(translations_file)
+    unless File.exists?(translate_filepath)
+      FileUtils.mkdir_p(File.dirname(translate_filepath))
+      File.open(translate_filepath, 'a'){}
+    end
+    unless d = YAML::load_file(translate_filepath)
+      I18n.available_locales.each do |l|
+        d ||= {}.merge!({ :"#{l.to_s}" => {:routes => {}} } )
+      end
+      File.write(translate_filepath, d.to_yaml)
+    end
   end
 end
 
