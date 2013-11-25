a23870e3aa22d29e1714e4299bbf0a4e2a4268f1
NicoArbogast <nicolas.arbogast@gmail.com>
2013-11-25 12:04:15 +0100

    works with rails 4

--- lib/localyzed/rack_rewrite.rb
+++ lib/localyzed/rack_rewrite.rb
@@ -2,7 +2,7 @@ require 'rack/rewrite'
 class Localyzed::Railtie < Rails::Railtie
   initializer "localized2.configure" do |app|
     app.class.configure do
-      config.middleware.insert_before(Rack::Lock, Rack::Rewrite) do
+      config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
         r301 %r{^(\/.*|)\/(en|fr)($|\?[^\\]*)}, '/$1/$2/$3' # redirect '/en' to '/en/'
       end
     end
--- lib/localyzed/version.rb
+++ lib/localyzed/version.rb
@@ -1,3 +1,3 @@
 module Localyzed
-  VERSION = "0.0.3.9"
+  VERSION = "0.0.4"
 end
